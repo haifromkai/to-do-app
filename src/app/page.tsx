6e2755d45e9e7f180ac7773a751701a80d4864ac
Kai Fang <49883801+haifromkai@users.noreply.github.com>
2024-06-21 16:28:39 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Next JS Starter</h1>
+      </div>
+    </section>
+  )
+}
+
