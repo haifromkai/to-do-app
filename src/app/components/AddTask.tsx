40980a9290092c1594bb8ce93f5372d38a3b1172
haifromkai <kf292@cornell.edu>
2024-06-21 18:49:14 -0700

    Added Modal.tsx component for AddTask pop up

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import { AiOutlinePlus } from "react-icons/ai";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
     <div>
-        <button className="btn btn-primary w-full">
-            ADD NEW TASK <AiOutlinePlus className="ml-2" size={18}/>
+        <button
+          onClick={() => setModalOpen(true)}
+          className="btn btn-primary w-full"
+        >
+          ADD NEW TASK <AiOutlinePlus className="ml-2" size={18}/>
         </button>
+
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+          modal for add todo
+        </Modal>
     </div>
   )
 }
